974d491965c132cb3b4056f9af8def8614a82d9e
MylardoChery <mylardochery@gmail.com>
2019-08-07 07:33:22 -0300

    My first Commit

---
+++ src/data/posts.js
@@ -0,0 +1,17 @@
+
+
+const posts = [{
+ id: 0,
+ description: "Squad took the bus",
+ imageLink: "https://scontent.faep9-1.fna.fbcdn.net/v/t1.0-9/18403313_677300112477875_8837453522386454527_n.jpg?_nc_cat=109&_nc_oc=AQk2Q2fbEBvliPADU_dD3nPpyRjm0yjVuZGjIRXJEs9kTAhSdSgmMCgSU1R3JsWy898&_nc_ht=scontent.faep9-1.fna&oh=9bdef1c574bc0a26e15819f3ad57c324&oe=5DE7B72B"
+ }, {
+ id: 1,
+ description: "Haitian FLag Day",
+ imageLink: "https://scontent.faep9-2.fna.fbcdn.net/v/t1.0-9/18556293_10213122807804425_5871448848009125961_n.jpg?_nc_cat=106&_nc_oc=AQkrDw_16wEofB3Cs8eG9lWjQTnFG5jzaHhdB-ou1fU1Zl8dxyjOdqD-2rGcR8vaU-8&_nc_ht=scontent.faep9-2.fna&oh=2df93cfb604329b1d74e07cfcd6cde2e&oe=5DA1EE7E"
+ }, {
+ id: 2,
+ description: "The team",
+ imageLink: "https://scontent.faep9-2.fna.fbcdn.net/v/t1.0-9/10659157_10152780216175460_7559339976519981602_n.jpg?_nc_cat=106&_nc_oc=AQlJPIFvXr1XSauYa4dScJWROdAGGM6pMegOzUs0YKwTwypFLcZVXqng7Nt8s-ARWdk&_nc_ht=scontent.faep9-2.fna&oh=29349e22b7601edc43317f6aa8b878f9&oe=5DEAE0C6"
+ }]
+
+ export default posts
